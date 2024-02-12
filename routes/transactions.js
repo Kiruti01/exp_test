0d55caf8cf9751af54d6d694734369beb3a47911
Michael Kiruti <mikekirutic@gmail.com>
2024-02-12 10:35:29 +0300

    code added
    testing deploy

---
+++ routes/transactions.js
@@ -0,0 +1,29 @@
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+} = require("../controllers/expense");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/income");
+
+const {
+  loginController,
+  registerController,
+} = require("../controllers/userController");
+
+const router = require("express").Router();
+
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpense)
+  .delete("/delete-expense/:id", deleteExpense)
+  .post("/login", loginController)
+  .post("/register", registerController);
+
+module.exports = router;
